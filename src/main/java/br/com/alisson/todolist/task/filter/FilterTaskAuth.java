d6047963888e7f8b0907e6277bf29f146132e98f
alissonjosef <alissonjosef@gmail.com>
2023-10-13 21:33:29 -0300

    adjust filter method get put

--- src/main/java/br/com/alisson/todolist/task/filter/FilterTaskAuth.java
+++ src/main/java/br/com/alisson/todolist/task/filter/FilterTaskAuth.java
@@ -25,44 +25,40 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                String requestURI = request.getRequestURI();
+        var servletPath = request.getServletPath();
 
-                if (requestURI.startsWith("/task")) {
+        if (servletPath.startsWith("/task/")) {
 
-                    var authorization = request.getHeader("Authorization");
-                    System.out.println(authorization);
-                    var authEncoded = authorization.substring("Basic".length()).trim();
+            var authorization = request.getHeader("Authorization");
+           
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecode);
+
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
             
-                    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-            
-                    var authString = new String(authDecode);
-            
-                    String[] credentials = authString.split(":");
-                    String username = credentials[0];
-                    String password = credentials[1];
-                    System.out.println(username);
-                    System.out.println(password);
-            
-                    var user =  this.userRepository.findByUsername(username);
-                    if (user == null) {
-                        response.sendError(401);
-                    }else {
-            
-                       var passwordVeryfy =  BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-            
-                      if( passwordVeryfy.verified) {
-                          
-                          filterChain.doFilter(request, response);
-                      }else {
-                        response.sendError(401);
-                      }
-            
-                    }
-                }else {
-                     filterChain.doFilter(request, response);
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+
+                var passwordVeryfy = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVeryfy.verified) {
+                    request.setAttribute("idUser", user.getId());
+
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
                 }
 
-
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
 
     }
 
